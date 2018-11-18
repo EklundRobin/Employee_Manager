0367b849763ceb4caf9dbfdb8b2672374b7ae488
Robin Eklund <robineklund6@gmail.com>
2018-11-18 13:02:56 +0200

    First commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyC7Ylh-ZYtCnZFVBumeFs7_H2dQLUGPVGw',
+  authDomain: 'vuefs-prod-33ce7.firebaseapp.com',
+  databaseURL: 'https://vuefs-prod-33ce7.firebaseio.com',
+  projectId: 'vuefs-prod-33ce7',
+  storageBucket: 'vuefs-prod-33ce7.appspot.com',
+  messagingSenderId: '910791356721'
+};
